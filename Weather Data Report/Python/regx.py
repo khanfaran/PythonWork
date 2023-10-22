582994c4b1c4122571ba53c57bb4fd6ac881b9a7
khanfaran <148684516+khanfaran@users.noreply.github.com>
2023-10-22 16:04:15 +0500

    Project Added

---
+++ Weather Data Report/Python/regx.py
@@ -0,0 +1,26 @@
+
+'\t\t{\t\t\t"@context": "http://schema.org",\t\t\t"@type": "Product",\t\t\t"name": "Moon bottle opener - FUTAGAMI",\t\t\t"image": "//cdn.shopify.com/s/files/1/0007/0051/4360/products/BOTTL-M93006_RAA_00.jpg?v=1573833429",\t\t\t"description": "A.P.C. now carries the brass \\"moon\\" bottle opener by FUTAGAMI. Solid brass. Crescent moon-shaped bottle opener. Smooth surface. Diameter: 2.56\\". Height: 0.4\\".",\t\t\t"brand": "APC",\t\t\t"sku": "BOTTL-M93006_RAA_OS",\t\t\t"offers": {\t\t\t\t"@type": "Offer",\t\t\t\t"url": "https://www.apc-us.com/products/bottl-m93006",\t\t\t\t"priceCurrency": "USD",\t\t\t\t"price": "75.00",\t\t\t\t"availability": "http://schema.org/InStock",\t\t\t\t"itemCondition": "https://schema.org/NewCondition"\t\t\t}\t\t}\t'
+
+'\t\t{\t\t\t"@context": "http://schema.org",\t\t\t"@type": "Product",\t\t\t"name": ""Moon" bottle opener - FUTAGAMI",\t\t\t"image": "//cdn.shopify.com/s/files/1/0007/0051/4360/products/BOTTL-M93006_RAA_00.jpg?v=1573833429",\t\t\t"description": "A.P.C. now carries the brass \\"moon\\" bottle opener by FUTAGAMI. Solid brass. Crescent moon-shaped bottle opener. Smooth surface. Diameter: 2.56\\". Height: 0.4\\".",\t\t\t"brand": "APC",\t\t\t"sku": "BOTTL-M93006_RAA_OS",\t\t\t"offers": {\t\t\t\t"@type": "Offer",\t\t\t\t"url": "https://www.apc-us.com/products/bottl-m93006",\t\t\t\t"priceCurrency": "USD",\t\t\t\t"price": "75.00",\t\t\t\t"availability": "http://schema.org/InStock",\t\t\t\t"itemCondition": "https://schema.org/NewCondition"\t\t\t}\t\t}\t'
+>>> 
+
+
+
+
+
+
+'\t\t{\t\t\t"@context": "http://schema.org",\t\t\t"@type": "Product",\t\t\t"name": "Moon bottle opener - FUTAGAMI",\t\t\t"image": "//cdn.shopify.com/s/files/1/0007/0051/4360/products/BOTTL-M93006_RAA_00.jpg?v=1573833429",\t\t\t"description": "A.P.C. now carries the brass \\"moon\\ bottle opener by FUTAGAMI. Solid brass. Crescent moon-shaped bottle opener. Smooth surface. Diameter: 2.56\\". Height: 0.4\\".",\t\t\t"brand": "APC",\t\t\t"sku": "BOTTL-M93006_RAA_OS",\t\t\t"offers": {\t\t\t\t"@type": "Offer",\t\t\t\t"url": "https://www.apc-us.com/products/bottl-m93006",\t\t\t\t"priceCurrency": "USD",\t\t\t\t"price": "75.00",\t\t\t\t"availability": "http://schema.org/InStock",\t\t\t\t"itemCondition": "https://schema.org/NewCondition \t\t}\t\t}\t'
+
+
+
+
+
+
+
+
+
+
+
+
+
+'\t\t{\t\t\t"@context": "http://schema.org",\t\t\t"@type": "Product",\t\t\t"name": "Moon bottle opener - FUTAGAMI",\t\t\t"image": "//cdn.shopify.com/s/files/1/0007/0051/4360/products/BOTTL-M93006_RAA_00.jpg?v=1573833429",\t\t\t"description": "A.P.C. now carries the brass \\"moon\\" bottle opener by FUTAGAMI. Solid brass. Crescent moon-shaped bottle opener. Smooth surface. Diameter: 2.56\\". Height: 0.4\\".",\t\t\t"brand": "APC",\t\t\t"sku": "BOTTL-M93006_RAA_OS",\t\t\t"offers": {\t\t\t\t"@type": "Offer",\t\t\t\t"url": "https://www.apc-us.com/products/bottl-m93006",\t\t\t\t"priceCurrency": "USD",\t\t\t\t"price": "75.00",\t\t\t\t"availability": "http://schema.org/InStock",\t\t\t\t"itemCondition": "https://schema.org/NewCondition"\t\t\t}\t\t}\t'
